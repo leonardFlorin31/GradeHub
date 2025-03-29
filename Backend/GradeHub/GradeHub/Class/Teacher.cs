d5528cc7917e31edc19002f209f0bfa49762812c
Constantin Florin-George <constantingeorge150@gmail.com>
2025-03-29 19:13:02 +0200

    teacher class

---
+++ Backend/GradeHub/GradeHub/Class/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using Person = global.GradeHub.MainClasses.Person;
+
+namespace global::GradeHub.MainClasses;
+
+public class Teacher : Person
+{
+    public string ClassId { get; set; }
+    
+    public Teacher(string firstName, string lastName, int age, string classId) : base(firstName, lastName, age)
+    {
+        ClassId = classId;
+    }
+}
